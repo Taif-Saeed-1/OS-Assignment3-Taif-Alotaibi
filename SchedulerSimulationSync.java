ec3e1f1393216f6b3c9e71658dadbdf62d95ffee
Taif-Saeed-1 <445052186@std.psau.edu.sa>
2026-05-05 16:59:49 +0300

    Protect waiting time calculation

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -73,7 +73,12 @@ class SharedResources {
     // Method to add waiting time
     public static void addWaitingTime(long time) {
         // TODO: Protect this critical section with a lock
-        totalWaitingTime += time;
+        counterLock.lock();
+        try {
+            totalWaitingTime += time;
+        } finally {
+            counterLock.unlock();
+        }
     }
 
     // Method to log execution
